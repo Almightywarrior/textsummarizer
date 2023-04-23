4b4032695788446e707c8b86cb2b776dee2ed2d6
varun-11-maker <varunsinghal2003@gmail.com>
2023-04-23 10:52:00 +0530

    demo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Almightywarrior/textsummarizer", "_blank")
           }
           className='black_btn'
         >
@@ -21,10 +21,10 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='orange_gradient '>OpenAI Summarizer</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with Sumz, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
